4d80ff7d1bfd943c8f99e7f29235657a014b5f86
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-08-07 16:58:02 -0300

    imagens / js
    incrementando o javascript e adicionando imagens.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert('Olá')
+function carregar(){
+  var msg = document.getElementById('msg')
+  var img = document.getElementById('imagem')
+
+  var data = new Date()
+  var hora = data.getHours()
+
+  msg.innerHTML = `Agora são exatamente ${hora} horas.`
+
+  if(hora >= 0 && hora < 12){
+    //foto manha!
+    img.src = 'imagens/amanhecer1.png'
+    document.body.style.background = '#80ccff'
+  }else if(hora >= 12 && hora <= 18){
+    //foto tarde!
+    img.src = 'imagens/entardecer1.png'
+    document.body.style.background = '#ffb366'
+  }else{
+    //foto noite!
+    img.src = 'imagens/noite1.png'
+    document.body.style.background = '#9966ff'
+  }
+}
